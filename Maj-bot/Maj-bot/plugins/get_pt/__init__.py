43d065f57e215b40ff6664f37723f195c9f9f85e
ygsldr <ygsldr@LAPTOP-NTS96PAH>
2025-05-03 21:59:09 +0800

    fix bugs for mspt

--- Maj-bot/Maj-bot/plugins/get_pt/__init__.py
+++ Maj-bot/Maj-bot/plugins/get_pt/__init__.py
@@ -1,17 +1,15 @@
-from nonebot import on_command
+from nonebot import on_command, on_message
 from nonebot.rule import to_me, startswith
 from nonebot.params import CommandArg
-from nonebot.adapters import Message
+from nonebot.adapters.onebot.v11 import Event, Bot, Message
 
 from .get_mspt import get_majsoul_pt
 
 mspt = on_message(rule=startswith("mspt"), priority=10)
 
 @mspt.handle()
-async def handle_function(args: Message = CommandArg()):
-    # 提取参数纯文本作为地名，并判断是否有效
-    if nickname := args.extract_plain_text():
-        pt_result = get_majsoul_pt(nickname)
-        await mspt.finish(f"{pt_result}")
-    else:
-        await mspt.finish("请输入雀魂用户名")
+async def handle_function(bot: Bot, event: Event):
+    message = event.get_message().extract_plain_text()
+    nickname = message[4:].strip()
+    pt_result = get_majsoul_pt(nickname)
+    await mspt.finish(f"{pt_result}")
